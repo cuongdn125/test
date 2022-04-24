be22593dcad7350378485b3c16dc2bffdb36fec1
cuongdn125 <ngoccuong061@gmail.com>
2022-04-24 17:12:05 +0700

    main

---
+++ src/components/__test__/App.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "../../App";
+
+it("Test App Component", () => {
+  const div = document.createElement("div");
+
+  expect(div.textContent).toEqual("App Component");
+
+  ReactDom.unmountComponentAtNode(div);
+});
